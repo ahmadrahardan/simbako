a6436e71d31d0ccde8645d183f6bc6f7c6834731
Rahardan <rahardan313@gmail.com>
2025-04-12 10:37:44 +0700

    sprint-1-3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if($this->app->environment('railway')){
+            URL::forceScheme('https');
+        }
+        config(['app.locale' => 'id']);
+        Carbon::setLocale('id');
     }
 }
